e5eac9a78dfff469cd46cbb6ecc32b625ccc2b5d
Kevin Barkan <kevin@4kg.com>
2017-03-02 18:37:09 -0500

    Adding starter code for the Hot Restaurant App

---
+++ app/data/waitingList.js
@@ -0,0 +1,15 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the waitlist tables.
+// Initially we just set it equal to a "dummy" customer.
+// But you could have it be an empty array as well.
+// ===============================================================================
+
+var waitingArray = [
+	// objects representing customers on the waiting list go here
+	// you may want to start with some hard-coded for testing
+];
+
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = waitingArray;
